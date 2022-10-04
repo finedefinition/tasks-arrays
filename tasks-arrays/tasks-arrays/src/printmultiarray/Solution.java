c16ed21dea7c3567a2a968ece30a1f901d18aaac
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-10-04 12:46:17 +0100

    Task10 solved

---
+++ tasks-arrays/tasks-arrays/src/printmultiarray/Solution.java
@@ -0,0 +1,38 @@
+package printmultiarray;
+
+/*
+В методе main(String[]) выведи на экран все числа из трехмерного
+массива multiArray.
+
+Требования:
+•	В классе Solution должна быть публичная статическая переменная
+multiArray типа int[][][].
+•	В методе main(String[]) выведи на экран все числа из multiArray.
+*/
+
+public class Solution {
+
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}},
+            {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+        //my task solution
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k] + " "); }
+                }
+            System.out.println();
+                }
+        //another solution with separator line
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("----------");
+        }
+            }
+        }
