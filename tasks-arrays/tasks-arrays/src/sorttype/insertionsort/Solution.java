0cdea9339ba8b2636318d8189a09655a1aa95e0a
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-10-14 10:53:32 +0100

    shuttle sorting type was added

--- tasks-arrays/tasks-arrays/src/sorttype/insertionsort/Solution.java
+++ tasks-arrays/tasks-arrays/src/sorttype/insertionsort/Solution.java
@@ -1,26 +1,30 @@
 package sorttype.insertionsort;
-
+// Сортировка вставкой - определяем положение для вставки
+// каждого следующего объекта больше/меньше  в новом массиве
+//Сложность O(n^2)
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         int[] array = {10, 2, 10, 3, 1, 2, 5};
         System.out.println(Arrays.toString(array));
-        boolean needIteration = true;
-        while (needIteration) {
-            needIteration = false;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
-                    swap(array, i, i-1);
-                    needIteration = true;
+        for (int left = 0; left < array.length; left++) {
+            // Вытаскиваем значение элемента
+            int value = array[left];
+            // Перемещаемся по элементам, которые перед вытащенным элементом
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                // Если вытащили значение меньшее — передвигаем больший элемент дальше
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    // Если вытащенный элемент больше — останавливаемся
+                    break;
                 }
             }
+            // В освободившееся место вставляем вытащенное значение
+            array[i + 1] = value;
         }
         System.out.println(Arrays.toString(array));
-    }
-    static void swap(int[] array, int ind1, int ind2) {
-        int tmp = array[ind1];
-        array[ind1] = array[ind2];
-        array[ind2] = tmp;
     }
 }
---
+++ tasks-arrays/tasks-arrays/src/sorttype/shuttlesort/Solution.java
@@ -0,0 +1,29 @@
+package sorttype.shuttlesort;
+
+import java.util.Arrays;
+
+// Челночная Сортировка  - проходим блоком (челноком) из 2-х чисел вперед по массиву
+//и сравниваем с отсортироованной частью
+//Сложность O(n^2)
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = {10, 2, 10, 3, 1, 2, 5};
+        System.out.println(Arrays.toString(array));
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i-1);
+                    needIteration = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    static void swap(int[] array, int ind1, int ind2) {
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+}
