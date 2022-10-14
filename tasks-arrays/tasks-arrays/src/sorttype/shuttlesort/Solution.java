f421e9a0d8791bf7a3d9859b0151eba108494583
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-10-14 11:44:11 +0100

    merge sorting type was added

--- tasks-arrays/tasks-arrays/src/sorttype/shuttlesort/Solution.java
+++ tasks-arrays/tasks-arrays/src/sorttype/shuttlesort/Solution.java
@@ -9,13 +9,15 @@ public class Solution {
     public static void main(String[] args) {
         int[] array = {10, 2, 10, 3, 1, 2, 5};
         System.out.println(Arrays.toString(array));
-        boolean needIteration = true;
-        while (needIteration) {
-            needIteration = false;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
-                    swap(array, i, i-1);
-                    needIteration = true;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                swap(array, i, i - 1);
+                for (int z = i - 1; (z - 1) >= 0; z--) {
+                    if (array[z] < array[z - 1]) {
+                        swap(array, z, z - 1);
+                    } else {
+                        break;
+                    }
                 }
             }
         }
